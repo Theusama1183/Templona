3f85d4304c0219220543daea40303ac314cb66ba
Theusama1183 <usamaadeel1183@gmail.com>
2022-12-29 14:47:06 +0500

    Change package name "Templona" to "Watchman"

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title ? title + " | Templona" : "Templona"}</title>
+        <title>{title ? title + " | Watchman" : "Watchman"}</title>
         <meta name="description" content="Ecommrece Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
