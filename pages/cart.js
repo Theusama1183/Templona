74d8703b2306d3203e131cbdf2a48eb802dd82b3
Theusama1183 <usamaadeel1183@gmail.com>
2022-12-31 03:18:36 +0500

    huj

--- pages/cart.js
+++ pages/cart.js
@@ -19,6 +19,10 @@ export default function Cart() {
     (a, c) => a + (c.quantity * c.price * 2.5) / 100,
     0
   );
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
+  };
   return (
     <Layout title="Cart">
       <div className="container m-auto px-4 mt-40">
@@ -55,7 +59,20 @@ export default function Cart() {
                           </div>
                         </Link>
                       </td>
-                      <td className="p-5 text-right">{item.quantity}</td>
+                      <td className="p-5 text-right">
+                        <select
+                          value={item.quantity}
+                          onChange={(e) =>
+                            updateCartHandler(item, e.target.value)
+                          }
+                        >
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
                       <td className="p-5 text-right">${item.price}</td>
                       <td className="p-5 text-center">
                         <button onClick={() => removeItemHandler(item)}>
